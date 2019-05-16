e81e6aee09aa3d27f88d724dedeec46a58a5acc5
afshinhatampour <afshin89hatampour@gmail.com>
2019-05-17 00:35:04 +0430

    Add author info and question

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+    public function getUrlAttribute()
+    {
+        return 'not set';
+        //return route('user.show', $this->id);
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -12,7 +12,17 @@
                         @foreach($questions as $question)
                             <div class="card mb-3 q-card">
                                 <div class="card-header q-card-title">
-                                    {{ $question->title  }}
+                                    <h3>
+                                        <a href="{{ $question->url }}"> {{ $question->title  }} </a>
+                                    </h3>
+                                    <p class="lead">Asked by :
+                                        <a href="{{ $question->user->url }}">
+                                            {{ $question->user->name }}
+                                        </a>
+                                        <small class="text-muted">
+                                            {{ $question->created_date }}
+                                        </small>
+                                    </p>
                                 </div>
                                 <div class="card-body q-card-body">
                                     {{ str_limit($question->body, 250) }}
